803144ec5ebb0ae3ec637b40fbc48cc4b6103bf7
Spikree <avimahalingpure10@gmail.com>
2023-12-24 13:30:05 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,6 +17,7 @@ class MyApp extends StatelessWidget {
         body: SafeArea(
           //avoids all the notches and things in a mobile device SafeArea
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50,
@@ -47,55 +50,52 @@ class MyApp extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                     color: Colors.teal[100]),
               ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                 child: Padding(
-                  padding: const EdgeInsets.all(17),
-                  child: Row(
-                    children: [
-                      Icon(
+                    padding: const EdgeInsets.all(17),
+                    child: ListTile(
+                      leading: Icon(
                         Icons.phone,
                         color: Colors.teal,
                         // size: 100,
                       ),
-                      SizedBox(width: 10),
-                      Text(
+                      title: Text(
                         '+44 123 456 789',
                         style: TextStyle(
                           fontSize: 20,
                           color: Colors.teal.shade900,
                           fontFamily: 'Source sans 3',
                         ),
-                      )
-                    ],
-                  ),
-                ),
+                      ),
+                    )),
               ),
               Card(
-                color: Colors.white,
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                 child: Padding(
-                  padding: const EdgeInsets.all(17),
-                  child: Row(
-                    children: [
-                      Icon(
+                    padding: const EdgeInsets.all(17),
+                    child: ListTile(
+                      leading: Icon(
                         Icons.email,
                         color: Colors.teal,
                         // size: 100,
                       ),
-                      SizedBox(width: 10),
-                      Text(
+                      title: Text(
                         'Avi@gmail.com',
                         style: TextStyle(
                           fontSize: 20,
                           color: Colors.teal,
                           fontFamily: 'Source sans 3',
                         ),
-                      )
-                    ],
-                  ),
-                ),
+                      ),
+                    )),
               ),
             ],
           ),
